99ffffdeffeaa83699a38530bc3330db94ea9154
Dogancan Kınık <dogancankinik@gmail.com>
2024-10-04 12:14:11 +0300

    person sınıfı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person=new Person( "John", "Doe",  20);
+
+        System.out.println("Firstname: " + person.getFirstName());
+
+        System.out.println("Lastname: " + person.getLastName());
+
+        System.out.println("Age: " + person.getAge());
+
+        System.out.println("Hobbies: " + person.getHobbies());
+
+        System.out.println("****************");
+
+        Person secondPerson=new Person("emrah","kızıltan",30, 1000, Boolean.TRUE, new String[]{"camping","reading"});
+
+        System.out.println("Firstname: " + secondPerson.getFirstName());
+
+        System.out.println("Lastname: " + secondPerson.getLastName());
+
+        System.out.println("Age: " + secondPerson.getAge());
+
+        System.out.println("Hobbies: " + Arrays.toString(secondPerson.getHobbies()));
     }
 }
